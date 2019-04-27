e8f7523012ba1341dfaa38be6c5af7197502f898
ZhaoRuiLong <zrl412@163.com>
2019-04-27 20:07:52 +0800

    feat: init

--- src/newby-shows-modules/rich-text-page/RichTextPage.vue
+++ src/newby-shows-modules/rich-text-page/RichTextPage.vue
@@ -4,42 +4,17 @@
   </div>
 </template>
 <script>
-
-/**
-   * @since 2019年4月13日15:10:19
-   * 新增控制页面元素显示功能：
-   * 使用方法为：
-   * 1.页面URL中包含visible参数，参数组成规则如下:
-   * 参数长度为三位，第一位控制标题是否显示，第二位控制作者是否显示，第三位控制更新时间是否显示。默认均不显示。每一位由0和1两位组成，0表示不显示，1表示显示。如:111则表示全部显示，000则表示不显示。
-   * 解析过程见： pageSectionVisible 方法
-   */
 export default {
   name: 'RichTextPageHome',
   data () {
-    return {
-      richTextContent: {},
-      loadCompleted: false,
-      dynamicStyle: {
-        margin: '4px auto'
-      },
-      imageInfo: {
-        width: 1,
-        height: 1
-      },
-      visible: {
-        title: false,
-        author: false,
-        updateTime: false
-      }
-    }
+    return {}
   },
   beforeCreate () {
   },
   created () {
 
   },
-  methods: {
-  }
+  methods: {}
 }
 </script>
 
